7fe6d81290c3bb14bea0d692777ac94b2209a9b6
Gowtam Paul <gowtamp@ivoyant.com>
2025-01-17 14:12:55 +0530

    added all images from platform-nx-docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
-import React from "react";
-import Layout from "@theme/Layout";
-import { TinaEditProvider } from "tinacms";
-import { Blocks } from "../components/Blocks";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import { SidebarSyncPlugin } from "../../plugins/SidebarSyncPlugin";
+import Layout from "@theme/Layout";
+import React from "react";
+import { Blocks } from "../components/Blocks";
 
 const pageData = require("../../config/homepage/index.json");
 
@@ -11,20 +9,15 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <TinaEditProvider showToolbar={true}>
-      <SidebarSyncPlugin />
-      <Layout
-        title={pageData && pageData.title ? pageData.title : siteConfig.title}
-        description={
-          pageData && pageData.description
-            ? pageData.description
-            : siteConfig.tagline
-        }
-      >
-        {pageData && pageData.blocks ? (
-          <Blocks blocks={pageData.blocks} />
-        ) : null}
-      </Layout>
-    </TinaEditProvider>
+    <Layout
+      title={pageData && pageData.title ? pageData.title : siteConfig.title}
+      description={
+        pageData && pageData.description
+          ? pageData.description
+          : siteConfig.tagline
+      }
+    >
+      {pageData && pageData.blocks ? <Blocks blocks={pageData.blocks} /> : null}
+    </Layout>
   );
 }
